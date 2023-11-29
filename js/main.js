52bdaaba03d20f7613e2264b9b23ba7e0fbc2cb4
Caio Carvalho <caio.simoes@aluno.senai.br>
2023-11-28 21:16:14 -0300

    alunos

--- js/main.js
+++ js/main.js
@@ -1,54 +1,100 @@
 // definindo um array de alunos
-
 let alunos = [
-    { nome: "Caio", email: "caio@senai.com", telefone: 2125588633, profissao: "FrontEnd" },
-    { nome: "Gabi", email: "gabi@senai.com", telefone: 2126739963, profissao: "FrontEnd" },
-    { nome: "Lucas", email: "lucas@senai.com", telefone: 2154584966, profissao: "FrontEnd" },
-    { nome: "Jaque", email: "jaque@gmail.com", telefone: 21974450073, profissao: "backend" },
-    { nome: "Julinha", email: "juju@msn.com", telefone: 21979984073, profissao: "fullstack" },
-    { nome: "Lucao", email: "lucao@hotmail.com", telefone: 21954454075, profissao: "mobile" },
-    { nome: "Inacio", email: "inacio@yahoo.com", telefone: 21977458473, profissao: "mobile" },
-    { nome: "Maurao", email: "maurao@hotmail.com", telefone: 21944488576, profissao: "frontend" },
-    { nome: "Ramonzao", email: "monzao@gmail.com", telefone: 2173484973, profissao: "frontend" },
-    { nome: "Davizao", email: "davi@msn.com", telefone: 21975584073, profissao: "frontend" },
+  {
+    nome: "Aninhha",
+    email: "ninha@gmail.com",
+    telefone: 21935484773,
+    profissao: "frontend"
+  },
+  { nome: "Jaque", email: "jaque@gmail.com", telefone: 21974450073, profissao: "backend" },
+  { nome: "Julinha", email: "juju@msn.com", telefone: 21979984073, profissao: "fullstack" },
+  { nome: "Lucao", email: "lucao@hotmail.com", telefone: 21954454075, profissao: "mobile" },
+  { nome: "Inacio", email: "inacio@yahoo.com", telefone: 21977458473, profissao: "mobile" },
+  { nome: "Maurao", email: "maurao@hotmail.com", telefone: 21944488576, profissao: "frontend" },
+  { nome: "Ramonzao", email: "monzao@gmail.com", telefone: 2173484973, profissao: "frontend" },
+  { nome: "Davizao", email: "davi@msn.com", telefone: 21975584073, profissao: "frontend" },
+
 ];
 
-//carregar os dados na tabela
-window.onload = (event)=> {
+// carregar os dados do array na tabela
+window.onload = (event) => {
   let tbody = document.querySelector("#myTable");
+  alunos.forEach((aluno, index) => {
+    let tr = document.createElement("tr");
+    tr.id = index +1;
 
-alunos.forEach((aluno, index) => {
-  let tr = document.createElement("tr");
+    let tdCod = document.createElement("td");
+    let tdNome = document.createElement("td");
+    let tdEmail = document.createElement("td");
+    let tdTelefone = document.createElement("td");
+    let tdProfissao = document.createElement("td");
+    let tdBotao = document.createElement("td");
 
-  let tdCod = document.createElement("td");
-  let tdNome = document.createElement("td");
-  let tdEmail = document.createElement("td");
-  let tdTelefone = document.createElement("td");
-  let tdProfissao = document.createElement("td");
-  let tdBotao = document.createElement("td");
+    tdCod.textContent = index;
+    tdNome.textContent = aluno.nome;
+    tdEmail.textContent = aluno.email;
+    tdTelefone.textContent = aluno.telefone;
+    tdProfissao.textContent = aluno.profissao;
+    tdBotao.innerHTML = `<button class="btn-remover" onclick="remover(this)" >Remover</button>`
 
-  tdCod.textContent = index;
-  tdNome.textContent = aluno.nome;
-  tdEmail.textContent = aluno.email;
-  tdTelefone.textContent = aluno.telefone;
-  tdProfissao.textContent = aluno.profissao;
-  tdBotao.innerHTML = `<button class="btn-remover" onclick="remover(this)">Remover</button>`;
+    tr.appendChild(tdCod);
+    tr.appendChild(tdNome);
+    tr.appendChild(tdEmail);
+    tr.appendChild(tdTelefone);
+    tr.appendChild(tdProfissao);
+    tr.appendChild(tdBotao);
 
-  tr.appendChild(tdCod);
-  tr.appendChild(tdNome);
-  tr.appendChild(tdEmail);
-  tr.appendChild(tdTelefone);
-  tr.appendChild(tdProfissao);
-  tr.appendChild(tdBotao);
+    tbody.appendChild(tr);
 
-  tbody.appendChild(tr);
-});
+
+  });
 
 
 }
 
-function remover (id){
-    let row = id.parentNode.parentNode.id;
-    row = document.getElementById(row);
-    
+function remover(id) {
+  let row = id.parentNode.parentNode.id;
+  row = document.getElementById(row);
+  row.parentNode.removeChild(row);
+  return false
+}
+
+function adicionar(){
+  //definindo as variaveis e recebendo dados
+  let nome = document.getElementById('nome').value;
+  let email = document.getElementById('email').value ;
+  let telefone = document.getElementById('telefone').value;
+  let profissao = document.getElementById('profissao').value;
+  let table = document.getElementById('myTable');
+
+
+  //calculando o  tamanho da tabela
+  let tableSize = table.rows.length;
+  //inserindo uma linha abaixo da tabela
+  let row = table.insertRow(tableSize);
+  let col1 = row.insertCell(0);
+  let col2 = row.insertCell(1);
+  let col3 = row.insertCell(2);
+  let col4 = row.insertCell(3);
+  let col5 = row.insertCell(4);
+  let col6 = row.insertCell(5);
+  //adicionando o id no elemento a ser criado
+  row.id = tableSize;
+//criando codigo do botão para remover a linha
+  let btnCode = "<button class='remove-btn' onclick='remover(this)'>Remover</button>";
+//preenchendo as celulas da linha
+col1.innerHTML = tableSize;
+col2.innerHTML = nome;
+col3.innerHTML = email;
+col4.innerHTML = telefone;
+col5.innerHTML = profissao;
+col6.innerHTML = btnCode;
+//Limpando os campos de inserção de dados
+document.getElementById('nome').value = "";
+document.getElementById('email').value = "";
+document.getElementById('telefone').value = "";
+document.getElementById('profissao').value = "";
+//Retornando 'false' para impedir o reload da pagina
+return false
+
 }
