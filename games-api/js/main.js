f1d6341e6f78718727c42845e21ec777353c985f
Caio Carvalho <caio.simoes@aluno.senai.br>
2023-12-12 21:31:10 -0300

    criar estrutura do arquivo index.html

--- games-api/js/main.js
+++ games-api/js/main.js
@@ -1,72 +1,53 @@
-import { getAllGames } from "./service.js";
 
-var URL = 'http://localhost:3000/jogos'
+import { getAllGames, creatGame, deleteGame, updateGame } from "./service.js";
+
 window.onload = () => {
     loadGames();
 };
 const loadGames = () => {
     console.log('>>>')
-    const dataContainer =
+    const dataContainer = 
         document.getElementById('data-container');
-    getAllGames().then(resp => {
+        getAllGames().then(resp =>{
         resp.forEach(game => {
-            const gamesElement =
+            const gamesElement = 
                 document.createElement('div');
-            gamesElement.innerHTML = `<strong>${game.nome}</strong><p>${game.preco}</p>`;
-            dataContainer.appendChild(gamesElement);
+                gamesElement.innerHTML = 
+`<strong>${game.nome}</strong><p>${game.preco}</p>`;
+             dataContainer.appendChild(gamesElement);
         });
     })
 };
 
-const createGame = () => {
-    const jogoTeste = {
+document.getElementById('btnCreate').addEventListener('click', ()=> {
+
+    const jogo = {
         "nome": "The legend of Zelda",
         "img": "https://codetheworld.io/wp-content/uploads/2023/12/Dark-Souls.png",
         "preco": 100
     };
-    fetch(URL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(jogoTeste)
-    })
-        .then(response => response.json())
-        .then(data => console.log('sucesso: ', data))
-        .catch((error) => console.log('Erro: ', error));
+    creatGame(jogo);
+});
 
-};
+document.getElementById('btnDelete')
+.addEventListener('click', ()=> {
+const jogo = {
+    "nome": "The legend of Zelda 2",
+    "img": "https://codetheworld.io/wp-content/uploads/2023/12/Dark-Souls.png",
+    "preco": 300,
+    "id": 3
+  };
+  deleteGame(jogo);
+});
 
-const deleteGame = () => {
-    const game = {
-        "nome": "The legend of Zelda",
+document.getElementById('btnUpdate')
+.addEventListener('click', ()=>{
+    const jogo = {
+        "nome": "The legend of Zelda 2",
         "img": "https://codetheworld.io/wp-content/uploads/2023/12/Dark-Souls.png",
-        "preco": 100,
+        "preco": 450,
         "id": 3
-    }
-    fetch(URL + `/${game.id}`, { method: 'DELETE' })
-        .then(response => response.json())
-        .then(data => console.log('sucesso: ', data))
-        .catch((error) => console.log('Erro: ', error));
+      };
 
-
-};
-
-const updateGame = () => {
-    const game = {
-        id: 3,
-        nome: "The legend of Zelda 2",
-        img: "https://codetheworld.io/wp-content/uploads/2023/12/Dark-Souls.png",
-        preco: 300,
-    };
-    fetch(URL + `/${game.id}`, {
-        method: 'PATCH', headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(game)
-    })
-        .then(response => response.json())
-        .then(data => console.log('sucesso: ', data))
-        .catch((error) => console.log('Erro: ', error));
-
-};
+      updateGame(jogo)
+});
--- games-api/js/service.js
+++ games-api/js/service.js
@@ -1,7 +1,8 @@
+//importar o arquivo que tem os metodos de exceptions
 import { handleErrors } from "./exception.js";
-var URL = 'http://localhost:3000/jogos'
-export const getAllGames = async () => {
+var URL = 'http://localhost:3000/jogos';
 
+export const getAllGames = async () => {
     try {
         // Fazendo uma solicitação GET para obter produtos da AP
         const response = await fetch(URL);
@@ -14,4 +15,36 @@ export const getAllGames = async () => {
     } catch (error) {
         console.log('Error >>>', error);
     }
+};
+
+export const creatGame = async (game) => {
+    fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(game)
+    })
+        .then(response => response.json())
+        .then(data => console.log('sucesso: ', data))
+        .catch((error) => console.log('Erro: ', error))
+};
+
+export const deleteGame = async (game) => {
+    fetch(URL + `/${game.id}`, { method: 'DELETE' })
+        .then(response => response.json())
+        .then(data => console.log('Success:', data))
+        .catch(error => console.error('Error:', error));
+}
+
+export const updateGame = async (game) => {
+    fetch(URL + `/${game.id}`, {
+        method: 'PATCH', headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(game)
+    })
+        .then(response => response.json())
+        .then(data => console.log('sucesso: ', data))
+        .catch((error) => console.log('Erro: ', error));
 }
